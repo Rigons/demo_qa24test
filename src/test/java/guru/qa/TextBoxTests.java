5842cb4a23729fa9e65765913efd0e950d437ad2
Roman Rastoropov <Roman.rastoropov@mail.ru>
2024-01-06 19:11:11 +0300

    1

--- src/test/java/guru/qa/TextBoxTests.java
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -15,29 +16,47 @@ public class TextBoxTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
     }
 
     @Test
     void fillFormTest() {
-        open("/text-box");
+        open("/automation-practice-form");
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Pirogov");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("gender-radio-1").click();
-        $("userNumber").setValue("0123456789");
-        $("form-control").setValue("13 Apr 1992");
-        $("hobbies-checkbox-3").click();
+        $("#userEmail").setValue("alex@pirogov.com");
+        $(".custom-control-label").click(); // выбор Gender = male
+        $("#userNumber").setValue("0123456789");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(4);
+        $(".react-datepicker__year-select").selectOption(92);
+        $(".react-datepicker__day--013").click();
+        $("#subjectsInput").setValue("e");
+        $("#subjectsWrapper").find(byText("English")).click();
+        $("#subjectsInput").setValue("h");
+        $("#subjectsWrapper").find(byText("History")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("snimok.PNG");
         $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Another street 1");
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        //result
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex Pirogov"));
+        $(".table-responsive").shouldHave(text("alex@pirogov.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("0123456789"));
+        $(".table-responsive").shouldHave(text("13 May,1992"));
+        $(".table-responsive").shouldHave(text("English, History"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("snimok.PNG"));
+        $(".table-responsive").shouldHave(text("Some street 1"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
     }
 }
