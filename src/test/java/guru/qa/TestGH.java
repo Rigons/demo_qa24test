7492d6501876b31a1256c34f461ca22aa09d659f
Roman Rastoropov <Roman.rastoropov@mail.ru>
2024-01-11 23:55:45 +0300

    version 1.0

---
+++ src/test/java/guru/qa/TestGH.java
@@ -0,0 +1,29 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestGH {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void checkEnterprise() {
+        open("https://github.com/");
+        $(new ByText("Solutions")).hover();
+        $(new ByText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" +
+                "developer platform."));
+    }
+}
